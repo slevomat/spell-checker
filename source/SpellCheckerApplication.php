4f8601b0a88a1ff85dba8fac88003420dd5bffce
Jan Langer <jan.langer@gmail.com>
2026-01-19 12:42:43 +0100

    Fixes from cs and phpstan

--- source/SpellCheckerApplication.php
+++ source/SpellCheckerApplication.php
@@ -26,6 +26,7 @@ use SpellChecker\Parser\PhpParser;
 use SpellChecker\Parser\PoParser;
 use Throwable;
 use Tracy\Debugger;
+use ReflectionNamedType;
 use function class_exists;
 use function count;
 use function implode;
@@ -39,8 +40,7 @@ use function sprintf;
 class SpellCheckerApplication
 {
 
-    /** @var Console */
-    private $console;
+    private Console $console;
 
     public function __construct(Console $console)
     {
@@ -103,18 +103,23 @@ class SpellCheckerApplication
 				}
 
 				$heuristicReflection = new ReflectionClass($heuristic);
-				$heuristicConstructor = $heuristicReflection->getConstructor();
-				if ($heuristicConstructor === null || count($heuristicConstructor->getParameters()) === 0) {
-					$heuristics[] = new $heuristic();
-				} else {
-					$parameters = $heuristicConstructor->getParameters();
-					if (count($parameters) !== 1 || $parameters[0]->getType()->getName() !== DictionaryCollection::class) {
-						$this->console->writeLn(C::red(sprintf('Custom heuristic class %s may have only one constructor parameter of type %s.', $heuristic, DictionaryCollection::class)));
-						exit(1);
-					}
-					$heuristics[] = new $heuristic($dictionaries);
-				}
-			}
+                $heuristicConstructor = $heuristicReflection->getConstructor();
+                if ($heuristicConstructor === null || count($heuristicConstructor->getParameters()) === 0) {
+                    $heuristics[] = new $heuristic();
+                } else {
+                    $parameters = $heuristicConstructor->getParameters();
+                    $firstParameterType = $parameters[0]->getType();
+                    if (
+                        count($parameters) !== 1
+                        || $firstParameterType === null
+                        || ($firstParameterType instanceof ReflectionNamedType && $firstParameterType->getName() !== DictionaryCollection::class)
+                    ) {
+                        $this->console->writeLn(C::red(sprintf('Custom heuristic class %s may have only one constructor parameter of type %s.', $heuristic, DictionaryCollection::class)));
+                        exit(1);
+                    }
+                    $heuristics[] = new $heuristic($dictionaries);
+                }
+            }
 
             // run check
             $spellChecker = new SpellChecker(
@@ -176,7 +181,7 @@ class SpellCheckerApplication
             if ($result->errorsFound()) {
                 exit(1);
             }
-        } catch (FileSearchNotConfiguredException $e) {
+        } catch (FileSearchNotConfiguredException) {
             $this->console->writeLn(C::red('Nothing to check. Configure directories or files.'));
             exit(1);
         } catch (Throwable $e) {
